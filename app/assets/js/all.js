b5bd2edda92fa10374f5856641a34113b0b60854
GillianY1 <gilliangilliany@gmail.com>
2021-08-28 02:49:48 +0800

    basic

---
+++ app/assets/js/all.js
@@ -0,0 +1,16 @@
+//import "../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"; 
+//import '../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+
+console.log('Hello!');
+
+$(document).ready(() => {
+  console.log('HesSchool Hello!')
+});
+
+/*
+let myModal = document.getElementById('addAdmin')
+let myInput = document.getElementById('addInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})*/
